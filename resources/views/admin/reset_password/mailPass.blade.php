b34b953d120a85b4a72066ea5b7261f9124c1cec
abdullah <abdullah.alhumsi9@gmail.com>
2023-10-18 16:01:10 +0300

    edit in pic in reset password

--- resources/views/admin/reset_password/mailPass.blade.php
+++ resources/views/admin/reset_password/mailPass.blade.php
@@ -2,63 +2,67 @@
 <!-- BEGIN: Body-->
 
 <body class="vertical-layout vertical-menu-modern blank-page navbar-floating footer-static  " data-open="click"
-      data-menu="vertical-menu-modern" data-col="blank-page">
-<!-- BEGIN: Content-->
-<div class="app-content content ">
+    data-menu="vertical-menu-modern" data-col="blank-page">
+    <!-- BEGIN: Content-->
+    <div class="app-content content ">
 
-    <div class="content-wrapper">
+        <div class="content-wrapper">
 
-        <div class="content-body container-fluid">
+            <div class="content-body container-fluid">
 
-            <div class="auth-wrapper auth-v2">
-                <div class="auth-inner row m-0">
-                    <!-- /Left Text-->
-                    <!-- Login-->
-                    <div class="d-flex col-lg-12 align-items-center auth-bg px-2 p-lg-5">
-                        <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
-                            <div class="d-flex justify-content-center">
-                                <img src="{{ asset('uploads/university_setting/' . $university_settings->logo) }}" alt="no-image">
-                            </div>
-                            @if (Session::has('message'))
-                                <div style="padding: 8px 15px;text-align: center;" class="alert alert-danger">{{ Session::get('message') }}</div>
-                            @endif
-
-                            <form class="auth-Active-form mt-2" method="POST" id="ActiveForm" action="{{ route('resetPass') }}">
-                                @csrf
-                                <div class="form-group">
-                                    <label class="form-label" for="email">@lang('admin.email')</label>
-                                    <input class="form-control" id="email" type="email" name="email"
-                                           placeholder="mail@mail.com" aria-describedby="email" autofocus=""
-                                           tabindex="1"/>
+                <div class="auth-wrapper auth-v2">
+                    <div class="auth-inner row m-0">
+                        <!-- /Left Text-->
+                        <!-- Login-->
+                        <div class="d-flex col-lg-12 align-items-center auth-bg px-2 p-lg-5">
+                            <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
+                                <div class="d-flex justify-content-center align-items-center"
+                                    style="height: 200px; width: 1350px; overflow: hidden;">
+                                    <img src="{{ asset('uploads/university_setting/' . $university_settings->logo) }}"
+                                        alt="no-image" style="max-width: 100%; max-height: 100%;">
                                 </div>
-                                <button type="submit" class="btn btn-primary btn-block btnActive-student"
+                                @if (Session::has('message'))
+                                    <div style="padding: 8px 15px;text-align: center;" class="alert alert-danger">
+                                        {{ Session::get('message') }}</div>
+                                @endif
+
+                                <form class="auth-Active-form mt-2" method="POST" id="ActiveForm"
+                                    action="{{ route('resetPass') }}">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label class="form-label" for="email">@lang('admin.email')</label>
+                                        <input class="form-control" id="email" type="email" name="email"
+                                            placeholder="mail@mail.com" aria-describedby="email" autofocus=""
+                                            tabindex="1" />
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-block btnActive-student"
                                         tabindex="4">Send</button>
-                            </form>
+                                </form>
+                            </div>
                         </div>
+                        <!-- /Login-->
                     </div>
-                    <!-- /Login-->
                 </div>
             </div>
         </div>
     </div>
-</div>
-<!-- END: Content-->
-<!-- BEGIN: Vendor JS-->
-<script src="{{ asset('app-assets') }}/vendors/js/vendors.min.js"></script>
-<!-- BEGIN Vendor JS-->
+    <!-- END: Content-->
+    <!-- BEGIN: Vendor JS-->
+    <script src="{{ asset('app-assets') }}/vendors/js/vendors.min.js"></script>
+    <!-- BEGIN Vendor JS-->
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.0/js/toastr.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.0/js/toastr.js"></script>
 
 
-<!-- BEGIN: Page Vendor JS-->
-<script src="{{ asset('app-assets') }}/vendors/js/forms/validation/jquery.validate.min.js"></script>
-<!-- END: Page Vendor JS-->
+    <!-- BEGIN: Page Vendor JS-->
+    <script src="{{ asset('app-assets') }}/vendors/js/forms/validation/jquery.validate.min.js"></script>
+    <!-- END: Page Vendor JS-->
 
-<!-- BEGIN: Theme JS-->
-<script src="{{ asset('app-assets') }}/js/core/app-menu.js"></script>
-<script src="{{ asset('app-assets') }}/js/core/app.js"></script>
-<!-- END: Theme JS-->
+    <!-- BEGIN: Theme JS-->
+    <script src="{{ asset('app-assets') }}/js/core/app-menu.js"></script>
+    <script src="{{ asset('app-assets') }}/js/core/app.js"></script>
+    <!-- END: Theme JS-->
 
-<!-- BEGIN: Page JS-->
-<script src="{{ asset('app-assets') }}/js/scripts/pages/page-auth-login.js"></script>
-<!-- END: Page JS-->
+    <!-- BEGIN: Page JS-->
+    <script src="{{ asset('app-assets') }}/js/scripts/pages/page-auth-login.js"></script>
+    <!-- END: Page JS-->
